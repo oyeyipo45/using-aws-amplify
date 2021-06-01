b53b248bec23493b1e80a8c07fccef38a4ab217a
damilola <oyeyipo45@gmail.com>
2021-06-01 09:34:28 +0100

    post complete

--- amplify/backend/function/userprofiles/src/index.js
+++ amplify/backend/function/userprofiles/src/index.js
@@ -4,17 +4,47 @@
 	STORAGE_USINGAWSAMPLIFY_ARN
 	STORAGE_USINGAWSAMPLIFY_NAME
 Amplify Params - DO NOT EDIT */
+//const { DynamoDB } = require('aws-sdk')
+const AWS = require('aws-sdk')
+const uuid = require('uuid')
+
+
+AWS.config.update({ region: process.env.REGION })
+const dynamodb = new AWS.DynamoDB.DocumentClient()
 
 exports.handler = async (event) => {
-    // TODO implement
+    let tableName = process.env.STORAGE_USINGAWSAMPLIFY_NAME;
+
+    if (event.httpMethod === 'POST') {
+        let putItemParams = {
+            TableName : tableName,
+            Item: {
+                'id': uuid.v4(),
+                'username': event.body.username,
+                'profilepicture': event.body.profilepicture
+            }
+        } 
+        try {
+            const newItem = await dynamodb.put(putItemParams).promise()
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+       let data = await dynamodb.scan({ TableName: tableName }).promise();
+       console.log(data);
+     
+    
+
     const response = {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+     headers: {
+         "Access-Control-Allow-Origin": "*",
+         "Access-Control-Allow-Headers": "*"
+     }, 
+        body: JSON.stringify(data),
     };
     return response;
 };
