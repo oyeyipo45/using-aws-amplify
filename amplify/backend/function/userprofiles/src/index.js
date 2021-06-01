764098d663d36fee197d625e635de3ca33523fbd
damilola <oyeyipo45@gmail.com>
2021-06-01 07:52:42 +0100

    amplify setup complete

---
+++ amplify/backend/function/userprofiles/src/index.js
@@ -0,0 +1,20 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	STORAGE_USINGAWSAMPLIFY_ARN
+	STORAGE_USINGAWSAMPLIFY_NAME
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
